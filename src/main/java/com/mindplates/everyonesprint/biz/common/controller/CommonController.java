31c1f07a1c63f862c79cd134f293f821f4c284e5
mindplates <mindplates@gmail.com>
2021-11-19 02:18:29 +0900

    스웨거 설정 추가 (/swagger-ui/index.html)

--- src/main/java/com/mindplates/everyonesprint/biz/common/controller/CommonController.java
+++ src/main/java/com/mindplates/everyonesprint/biz/common/controller/CommonController.java
@@ -2,6 +2,7 @@ package com.mindplates.everyonesprint.biz.common.controller;
 
 import com.mindplates.everyonesprint.biz.common.vo.response.SystemInfo;
 import com.mindplates.everyonesprint.framework.annotation.DisableLogin;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ public class CommonController {
 
     @DisableLogin
     @GetMapping("/system/info")
+    @Operation(summary = "API 버전 조회", description = "API 버전 조회")
     public SystemInfo selectSystemInfo() {
         return SystemInfo.builder().version(buildProperties.getVersion()).build();
     }
---
+++ src/main/java/com/mindplates/everyonesprint/framework/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.mindplates.everyonesprint.framework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+
+}
