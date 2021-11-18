a47cc060db670c9883c8169d3828eb63eb94a25e
mindplates <mindplates@gmail.com>
2021-11-19 02:46:06 +0900

    CORS 허용

--- src/main/java/com/mindplates/everyonesprint/framework/config/WebConfig.java
+++ src/main/java/com/mindplates/everyonesprint/framework/config/WebConfig.java
@@ -1,10 +1,21 @@
 package com.mindplates.everyonesprint.framework.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${everyone_sprint.corsUrls}")
+    private String[] corsUrls;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(this.corsUrls)
+                .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS").allowCredentials(true);
+    }
 
 
 
