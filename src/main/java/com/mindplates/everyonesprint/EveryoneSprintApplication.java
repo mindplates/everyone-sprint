1ceac88932955d47851d870a5e70ac2963fd2f38
mindplates <mindplates@gmail.com>
2022-03-18 00:39:10 +0900

    build

--- src/main/java/com/mindplates/everyonesprint/EveryoneSprintApplication.java
+++ src/main/java/com/mindplates/everyonesprint/EveryoneSprintApplication.java
@@ -2,16 +2,24 @@ package com.mindplates.everyonesprint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
-public class EveryoneSprintApplication {
+public class EveryoneSprintApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EveryoneSprintApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EveryoneSprintApplication.class);
+    }
+
 
     @PostConstruct
     public void started() {
