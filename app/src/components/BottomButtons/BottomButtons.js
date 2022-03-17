5d5200d79ea01f1abe5d8918b35c0b5a890cf6b6
mindplates <mindplates@gmail.com>
2022-03-17 23:24:44 +0900

    production 설정 추가

--- app/src/components/BottomButtons/BottomButtons.js
+++ app/src/components/BottomButtons/BottomButtons.js
@@ -47,7 +47,7 @@ const BottomButtons = (props) => {
           </div>
         )}
         {onClose && (
-          <div className='close-buttons text'>
+          <div className='close-buttons'>
             {onDelete && <Liner display="inline-block" width="1px" height="10px" color="light" margin="0 0.5rem" />}
             <Button className="close-button" type="button" size={size} color="warning" outline onClick={onClose}>
               <div>
